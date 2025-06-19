f910572777897e7a021cc8383448a66260a5d864
REscalonaJR <esca0063@student.acitech.org>
2025-06-19 11:09:04 -0400

    bi

--- bee.js
+++ bee.js
@@ -4,6 +4,11 @@ bee.addEventListener("click", () => {
 if (clicks == 0) {
 bee.classList.add("active");
 bee.classList.remove("deactive");
+setTimeout(() => {
+    if (clicks==2) {
+        bee.classList.add("spin")
+    }
+}, 3000);
 }
 clicks++
 })
