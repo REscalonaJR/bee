7565ce8c0c7aaf972a330f3725f91f4ce6efc3c7
REscalonaJR <esca0063@student.acitech.org>
2025-06-19 16:07:18 -0400

    uiipubu

--- bee.js
+++ bee.js
@@ -8,5 +8,14 @@ setTimeout(() => {
         bee.classList.add("spin")
 }, 3000);
 }
+if (clicks == 1,2,3) {
+        setTimeout(() => {
+                clicks--
+        }, 1000);
+let redness = clicks*0.166;
+let yup = "255,0,0,"+redness;
+bee.style.backgroundColor = yup
+bee.style.transition = "0.5s ease-in"
+}
 clicks++
 })
