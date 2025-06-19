81200ea71489b95c63ddd0e516aad21ba7e93ca2
REscalonaJR <esca0063@student.acitech.org>
2025-06-19 16:35:15 -0400

    lnoi

--- bee.js
+++ bee.js
@@ -1,11 +1,25 @@
 let bee = document.getElementById("bee")
 let clicks = 0
+let pester = 0
 bee.addEventListener("click", () => {
 if (clicks == 0) {
 bee.classList.add("active");
 bee.classList.remove("deactive");
 setTimeout(() => {
         bee.classList.add("spin")
+        bee.addEventListener("click", () => {
+pester++
+setTimeout(() => {
+        clicks--
+}, 1000);
+if (pester == 1) {
+        bee.style.backgroundColor = "rgba(255,0,0,0.166)"
+} else if (pester == 2) {
+        bee.style.backgroundColor = "rgba(255,0,0,0.333)"
+} else if (pester == 3) {
+        bee.style.backgroundColor = "rgba(255,0,0,0.500)"
+}
+        })
 }, 3000);
 }
 clicks++
