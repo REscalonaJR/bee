0f5ebcf4a176bf163e6a22ceaba9a1cc2611bab5
REscalonaJR <esca0063@student.acitech.org>
2025-06-19 16:27:02 -0400

    nonol

--- bee.js
+++ bee.js
@@ -8,19 +8,6 @@ setTimeout(() => {
         bee.classList.add("spin")
 }, 3000);
 }
-})
-if (clicks == 1,2,3) {
-        setTimeout(() => {
-                clicks--
-        }, 1000);
-if (clicks== 1) {
-        bee.style.backgroundColor = "rgba(255,0,0,0.166)"
-        
-} else if (clicks == 2) {
-        bee.style.backgroundColor = "rgba(255,0,0,0.332)"
-} else if (clicks == 3) {
-        bee.style.backgroundColor = "rgba(255,0,0,0.500)"
-}
 clicks++
-bee.style.transition = "0.5s ease-in"
-}
+})
+
