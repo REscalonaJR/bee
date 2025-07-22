4aebd09e045c6ff885595e6aeb7da20b38da2d4f
REscalonaJR <esca0063@student.acitech.org>
2025-07-22 15:52:48 -0400

    ewfew

--- bee.js
+++ bee.js
@@ -1,27 +1,13 @@
-let bee = document.getElementById("bee")
-let clicks = 0
-let pester = 0
-bee.addEventListener("click", () => {
-if (clicks == 0) {
-bee.classList.add("active");
-bee.classList.remove("deactive");
-setTimeout(() => {
-        bee.classList.add("spin")
-        bee.addEventListener("click", () => {
-pester++
-setTimeout(() => {
-        clicks--
-}, 1000);
-if (pester == 1) {
-        bee.style.backgroundColor = "rgba(255,0,0,0.166)"
-} else if (pester == 2) {
-        bee.style.backgroundColor = "rgba(255,0,0,0.333)"
-} else if (pester == 3) {
-        bee.style.backgroundColor = "rgba(255,0,0,0.500)"
+let num = document.getElementById("num")
+let resu = document.getElementById("resu")
+function calc(){
+        for (let index = 0; index < 10000; index++) {
+                let res = num.innerHTML/index
+                if (res%2==0, res%2==1) {
+                        resu.innerHTML = resu.innerHTML + " " + res
+                } else {
+                        
+                }
+                
+        }
 }
-        })
-}, 3000);
-}
-clicks++
-})
-
